dc55049ea4ffe773d4c5e4240ae55489925117c7
Décamps Laurent <laurent.decamps@diagngrow.com>
2023-12-22 14:13:32 +0100

    fix:  first refactoring

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,82 +1,93 @@
-namespace Tennis
+namespace Tennis;
+
+public class TennisGame1 : ITennisGame
 {
-    public class TennisGame1 : ITennisGame
+    private int _mScore1;
+    private int _mScore2;
+    private string player1Name;
+    private string player2Name;
+
+    public TennisGame1(string player1Name, string player2Name)
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
+        this.player1Name = player1Name;
+        this.player2Name = player2Name;
+    }
 
-        public TennisGame1(string player1Name, string player2Name)
+    public void WonPoint(string playerName)
+    {
+        if (playerName == "player1")
+            _mScore1 += 1;
+        else
+            _mScore2 += 1;
+    }
+
+    public string GetScore()
+    {
+        if (IsScoreEqual())
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            return GetEqualityScore();
         }
 
-        public void WonPoint(string playerName)
+        return IsOneScorePossiblyWin() ? 
+                GetAdvantageOrWin() : 
+                GetNonEqualityAdvantageOrWinScore();
+    }
+
+    private bool IsOneScorePossiblyWin()
+    {
+        return _mScore1 >= 4 || _mScore2 >= 4;
+    }
+
+    private bool IsScoreEqual()
+    {
+        return _mScore1 == _mScore2;
+    }
+
+    private string GetNonEqualityAdvantageOrWinScore()
+    {
+        return $"{GetNormalScore(_mScore1)}-{GetNormalScore(_mScore2)}";
+    }
+
+    private static string GetNormalScore(int score)
+    {
+        return score switch
         {
-            if (playerName == "player1")
-                m_score1 += 1;
-            else
-                m_score2 += 1;
+            0 => "Love",
+            1 => "Fifteen",
+            2 => "Thirty",
+            3 => "Forty",
+            _ => "error"
+        };
+    }
+
+    private string GetAdvantageOrWin()
+    {
+        var minusResult = _mScore1 - _mScore2;
+        switch (minusResult)
+        {
+            case 1:
+                return "Advantage player1";
+            case -1:
+                return "Advantage player2";
+            case >= 2:
+                return "Win for player1";
+            default:
+                return "Win for player2";
         }
+    }
 
-        public string GetScore()
+    private string GetEqualityScore()
+    {
+        switch (_mScore1)
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
-            {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
-            }
-            else if (m_score1 >= 4 || m_score2 >= 4)
-            {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
-            }
-            return score;
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
         }
     }
 }
-
