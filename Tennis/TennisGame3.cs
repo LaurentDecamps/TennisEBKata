695d272a72c58f78401d15eeb441c70824109803
Décamps Laurent <laurent.decamps@diagngrow.com>
2023-12-30 12:58:18 +0100

    chore: tennisGame3 refactoring in progress

--- Tennis/TennisGame3.cs
+++ Tennis/TennisGame3.cs
@@ -1,43 +1,72 @@
-namespace Tennis
+using System;
+
+namespace Tennis;
+
+public class TennisGame3 : ITennisGame
 {
-    public class TennisGame3 : ITennisGame
+    private int _player2Score;
+    private int _player1Score;
+    private string _player1Name;
+    private string _player2Name;
+    private const string Player1Name = "player1";
+    private static readonly string[] ClassicScoreList = { "Love", "Fifteen", "Thirty", "Forty" };
+
+
+    public TennisGame3(string player1Name, string player2Name)
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        this._player1Name = player1Name;
+        this._player2Name = player2Name;
+    }
 
-        public TennisGame3(string player1Name, string player2Name)
+    public string GetScore()
+    {
+        bool arePlayersScoreEquals = ArePlayersScoreEquals();
+        const string allString = "-All";
+        if (AreBothPlayerUnderFourPoint() && IsTotalScoreUnderSix())
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            string player1Score = ClassicScoreList[_player1Score];
+            return arePlayersScoreEquals ? $"{player1Score}{allString}"
+                : $"{player1Score}-{ClassicScoreList[_player2Score]}";
         }
 
-        public string GetScore()
-        {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
-            {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-            }
-            else
-            {
-                if (p1 == p2)
-                    return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
-            }
-        }
+        if (arePlayersScoreEquals)
+            return "Deuce";
+        string playerAboutToWin = PlayerAboutToWin();
+        return AreScoreSeparatedByOnePoint()
+            ? "Advantage " + playerAboutToWin
+            : "Win for " + playerAboutToWin;
+    }
 
-        public void WonPoint(string playerName)
-        {
-            if (playerName == "player1")
-                this.p1 += 1;
-            else
-                this.p2 += 1;
-        }
+    private bool AreScoreSeparatedByOnePoint()
+    {
+        return Math.Abs(_player1Score - _player2Score) == 1;
+    }
 
+    private string PlayerAboutToWin()
+    {
+        return _player1Score > _player2Score ? _player1Name : _player2Name;
+    }
+
+    private bool IsTotalScoreUnderSix()
+    {
+        return (_player1Score + _player2Score < 6);
+    }
+
+    private bool ArePlayersScoreEquals()
+    {
+        return (_player1Score == _player2Score);
+    }
+
+    private bool AreBothPlayerUnderFourPoint()
+    {
+        return (_player1Score < 4 && _player2Score < 4);
+    }
+
+    public void WonPoint(string playerName)
+    {
+        if (playerName == Player1Name)
+            this._player1Score += 1;
+        else
+            this._player2Score += 1;
     }
 }
